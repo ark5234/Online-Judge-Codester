33efab690058383763b224803a730cb028c6f6cd
Vikrant Kawadkar <114129298+ark5234@users.noreply.github.com>
2025-07-24 14:13:17 +0530

    implemented google auth with appwrite for backend

--- backend/server/src/api/auth/routes.js
+++ backend/server/src/api/auth/routes.js
@@ -1,11 +1,7 @@
 const express = require('express');
-const { register, login, googleAuth, getMe } = require('./controller');
-const { authenticateJWT } = require('../../middleware/auth');
 const router = express.Router();
+const { googleLogin } = require('./controller');
 
-router.post('/register', register);
-router.post('/login', login);
-router.get('/google', googleAuth); // Implement with passport.js
-router.get('/me', authenticateJWT, getMe);
+router.post('/google-login', googleLogin);
 
 module.exports = router; 
