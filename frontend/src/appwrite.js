fbcaa8da790437e1d28678b3d14a71fcbfc56ecc
Vikrant Kawadkar <114129298+ark5234@users.noreply.github.com>
2025-07-26 17:52:43 +0530

    redirect appwrite

--- frontend/src/appwrite.js
+++ frontend/src/appwrite.js
@@ -6,3 +6,9 @@ client
   .setProject('6881edab0015c849630d');
 
 export const account = new Account(client); 
+import { account } from './appwrite';
+
+export const loginWithGoogle = () => {
+  const redirectUrl = window.location.origin;
+  account.createOAuth2Session('google', redirectUrl, redirectUrl);
+};
